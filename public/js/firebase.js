89e883e99a2772aeff224c69df3207ee04fab581
Desocseronit <nesynovsavva7@gmail.com>
2026-03-30 23:19:12 +0300

    global clicker

---
+++ public/js/firebase.js
@@ -0,0 +1,49 @@
+const firebaseConfig = {
+    apiKey: "__FIREBASE_API_KEY__",
+    authDomain: "__FIREBASE_AUTH_DOMAIN__",
+    projectId: "__FIREBASE_PROJECT_ID__",
+    storageBucket: "__FIREBASE_STORAGE_BUCKET__",
+    messagingSenderId: "__FIREBASE_MESSAGING_SENDER_ID__",
+    appId: "__FIREBASE_APP_ID__"
+};
+
+
+import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
+import { 
+    getFirestore, doc, updateDoc, onSnapshot, increment, 
+    getDoc, setDoc 
+} from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js';
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const counterRef = doc(db, "counters", "global");
+
+const clickCountSpan = document.querySelector('#clicker span');
+const hiButton = document.querySelector('#clicker button');
+
+
+onSnapshot(counterRef, (docSnapshot) => {
+    if (docSnapshot.exists()) {
+        clickCountSpan.innerText = docSnapshot.data().count || 0;
+    }
+});
+
+
+hiButton.addEventListener('click', async () => {
+    try {
+        await updateDoc(counterRef, {
+            count: increment(1)
+        });
+    } catch (error) {
+        console.error("Ошибка:", error);
+    }
+});
+
+
+async function initCounter() {
+    const docSnap = await getDoc(counterRef);
+    if (!docSnap.exists()) {
+        await setDoc(counterRef, { count: 0 });
+    }
+}
+initCounter();
