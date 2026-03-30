87ce56d8d1dfa187684d062be1648c4851f2d23a
Desocseronit <nesynovsavva7@gmail.com>
2026-03-30 23:25:29 +0300

    some changes

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,11 +1,4 @@
-const firebaseConfig = {
-    apiKey: "__FIREBASE_API_KEY__",
-    authDomain: "__FIREBASE_AUTH_DOMAIN__",
-    projectId: "__FIREBASE_PROJECT_ID__",
-    storageBucket: "__FIREBASE_STORAGE_BUCKET__",
-    messagingSenderId: "__FIREBASE_MESSAGING_SENDER_ID__",
-    appId: "__FIREBASE_APP_ID__"
-};
+const firebaseConfig = __FIREBASE_CONFIG__;
 
 
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
