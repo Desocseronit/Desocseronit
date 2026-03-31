970501df5039881812365eb2cdebe9d4fba28643
Desocseronit <nesynovsavva7@gmail.com>
2026-03-31 12:58:06 +0300

    Add files via upload

---
+++ public/js/githubRepos.js
@@ -0,0 +1,34 @@
+const tBody = document.querySelector('#repo-table tbody')
+fetch('https://api.github.com/users/Desocseronit/repos?sort=updated')
+.then(resp => {
+    if(resp.status != 200){
+        tBody.querySelector('td').textContent = 'Error('
+        throw new Error(`Fetch failed with code ${resp.status}`)
+    }
+    else return resp.json()
+    
+})
+.then(arr => {tBody.innerHTML = '';
+    for(let i = 0; i < arr.length; i++){
+        drawNewRow(arr[i])
+    }
+})
+
+
+function drawNewRow(json){
+    console.log(json)
+    let tr = document.createElement('tr')
+    for(let i = 0; i < 2; i++){
+        let cell = tr.insertCell()
+        if(i == 0) cell.textContent = json.name
+        else {
+            let a = document.createElement('a')
+            console.log(json.html_url)
+            a.href = json.html_url
+            a.textContent = 'Link'
+            cell.appendChild(a)
+        }
+    }
+    console.log(tr.innerHTML)
+    tBody.appendChild(tr)
+}
