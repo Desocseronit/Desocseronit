84ee52ef131b7934b8da2953c10cc17f409cdbe0
Desocseronit <nesynovsavva7@gmail.com>
2026-04-01 20:58:52 +0300

    now remember page

--- public/js/pageController.js
+++ public/js/pageController.js
@@ -1,8 +1,13 @@
 const pageButtons = document.querySelectorAll('header #tabs div')
+if(localStorage.getItem('currentPage')){
+    document.querySelector('.root.show').classList.remove('show')
+    document.querySelector(`#${localStorage.getItem('currentPage')}`).classList.add('show')
+}
 
 pageButtons.forEach(button => {
     button.addEventListener('click' , e=>{
         document.querySelector('.root.show').classList.remove('show')
         document.querySelector(`#${e.target.dataset.target}`).classList.add('show')
+        localStorage.setItem('currentPage' , e.target.dataset.target)
     })
 })
