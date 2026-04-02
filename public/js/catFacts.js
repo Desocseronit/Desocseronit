bfea24ac1b15b1ca02c0f5014adf813b4a1ce0f5
Desocseronit <nesynovsavva7@gmail.com>
2026-04-02 11:22:08 +0300

    im durachok

--- public/js/catFacts.js
+++ public/js/catFacts.js
@@ -1,27 +1,27 @@
 const factSpan = document.querySelector('#fact')
 const button = document.querySelector('.root#page3 #cat-facts-container button')
 button.addEventListener('click', e => {
-    if (!button.classList.contains('disabled')) {
-        console.log
-        fetch('https://meowfacts.herokuapp.com/')
-            .then(resp => {
-                factSpan.textContent = ''
-                factSpan.classList.add('loader')
-                button.classList.add('disabled')
-                if (resp.status != 200) {
-                    factSpan.classList.remove('loader')
-                    button.classList.remove('disabled')
-                    factSpan.textContent = 'Error('
-                    throw new Error(`Fetch failed with code ${resp.status}`)
-                }
-                else {
-                    factSpan.classList.remove('loader')
-                    button.classList.remove('disabled')
-                    return resp.json()
-                }
-            })
-            .then(json => {
-                factSpan.textContent = json.data[0]
-            })
-    }
-})
+    if (button.classList.contains('disabled')) return;
+
+    button.classList.add('disabled');
+    factSpan.textContent = '';
+    factSpan.classList.add('loader');
+
+    fetch('https://meowfacts.herokuapp.com/')
+        .then(resp => {
+            if (!resp.ok) throw new Error(`Error: ${resp.status}`);
+            return resp.json();
+        })
+        .then(json => {
+            factSpan.textContent = json.data[0];
+        })
+        .catch(err => {
+            console.error(err);
+            factSpan.textContent = 'Error(';
+        })
+        .finally(() => {
+            factSpan.classList.remove('loader');
+            button.classList.remove('disabled');
+        });
+});
+
